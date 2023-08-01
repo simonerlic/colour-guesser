a44b3821bb7733bc09bf3779e49cedf50245b6bd
Simon Erlic <simon@erlic.ca>
2023-08-01 00:04:10 -0700

    Fix non-conformant AppBar colour

--- lib/views/results_page.dart
+++ lib/views/results_page.dart
@@ -40,13 +40,6 @@ class ResultsPage extends StatelessWidget {
 
     return Scaffold(
       appBar: AppBar(
-          backgroundColor:
-              Colors.white, //Theme.of(context).colorScheme.inversePrimary,
-          shape: const RoundedRectangleBorder(
-            borderRadius: BorderRadius.vertical(
-              bottom: Radius.circular(15),
-            ),
-          ),
           title: const Text("Results"),
           centerTitle: true,
           titleTextStyle: Theme.of(context).textTheme.headlineSmall,
