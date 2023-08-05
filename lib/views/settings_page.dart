dc2c83c76e800e446c3e2cdeb117660b87a81ecc
Simon Erlic <simon@erlic.ca>
2023-08-04 18:41:59 -0700

    Set up settings page

---
+++ lib/views/settings_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+// import 'package:shared_preferences/shared_preferences.dart';
+
+class SettingsPage extends StatefulWidget {
+  const SettingsPage({Key? key}) : super(key: key);
+
+  @override
+  _SettingsPageState createState() => _SettingsPageState();
+}
+
+class _SettingsPageState extends State<SettingsPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(appBar: AppBar());
+  }
+}
